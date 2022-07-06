822f5443f1733ef4e3c4c62758254efc79b7653d
Ellaabang <106862020+Ellaabang@users.noreply.github.com>
2022-07-06 23:40:06 +0100

    6t67

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_alphabet - function to print
  *
@@ -11,8 +10,7 @@
 
     for (c = 'a'; c <= 'z'; c++)
     {
-        _putchar(c)
+        _putchar(c);
     }
 _putchar('\n');
  }
- 
