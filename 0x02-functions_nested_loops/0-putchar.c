fb6443a73ff201084c2ca7711c375890a92cc076
Ellaabang <106862020+Ellaabang@users.noreply.github.com>
2022-07-06 16:35:50 +0100

    gygfy

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *main - print _putchar
+ * main - print _putchar
  *
- *Return: Always 0 (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-    _putchar('-');
+    _putchar('_');
     _putchar('p');
     _putchar('u');
     _putchar('t');
