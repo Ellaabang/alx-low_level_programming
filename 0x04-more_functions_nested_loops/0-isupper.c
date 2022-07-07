c2b26db53aa0d3159e649b93c5c665c270845963
Ellaabang <106862020+Ellaabang@users.noreply.github.com>
2022-07-08 00:57:51 +0100

    yygy

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - return 1 if c is an uppercase alphabet
+ *@c: the character to check
+ * Return: 0 (success)
+ */
+
+ int main(void)
+ {
+        int i;
+
+        for (i = 'A'; i <= 'Z'; i++)
+        {
+            if (i == c)
+            {
+                return (1);
+            }
+        }
+
+        return (0);
+ }
+ 
