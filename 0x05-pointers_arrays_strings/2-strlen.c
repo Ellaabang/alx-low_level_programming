7f182e5e35f3fbd12516d01fda6bb030d5903dd9
Ellaabang <106862020+Ellaabang@users.noreply.github.com>
2022-07-12 23:23:02 +0100

    fv

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,16 @@
 
 /**
  * _strlen - returns the length of a string
- * 
- * Description: strlen returns length of a string
+ * @s: parameter
+ *
+ * Description: returns the length of the string
+ * Return: the length of the string
  */
-
 int _strlen(char *s)
 {
-    int l = 0;
+	int l = 0;
 
-    while (*s++)
-     l++;
-     return (l);
+	while (*s++)
+		l++;
+	return (l);
 }
