56c42fd0856da4a8952d29cc4f614863a26e6ba7
Ellaabang <106862020+Ellaabang@users.noreply.github.com>
2022-07-21 01:17:41 +0100

    hgh

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - scans for string for any set of bytes
+ * @x: base
+ * @y: raised to the power
+ * Return: pointer to the byte that matches
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x* _pow_recursion(x, y - 1));
+}
